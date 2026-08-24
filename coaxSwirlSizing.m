995c31bf33038354ff7fe40ac6f2e30a5a54edf9
kyleslactaoen <kyleslactaoen@g.ucla.edu>
2026-08-23 21:54:58 -0700

    Working Stage One, Geometry Check and Report

--- coaxSwirlSizing.m
+++ coaxSwirlSizing.m
@@ -1,6 +1,6 @@
 % Sizing Script for Coaxial Swirl Injector Elements
-% Written for internal mixing types (stage 1 nozzle recessed into stage 2)
-% Stage 1, OX; Stage 2, Fuel
+% Written for externally mixing types (spray cones do not mix before
+% nozzle)
 
 clc; clear; close all;
 
@@ -30,15 +30,16 @@ Chart.alpha = interp1(Chart.Aalpha, alphaU, Chart.AQalpha, 'pchip');
 
 % Design Parameters
 
-Param.numElements = 9;
+Param.numElements = 4;
+Param.minFlangeThickness = 0;
+Param.wallThickness = 0;
 
     % Imperial
 Param.PcUS = 370; % Psia
 Param.mdotTotUS = 6.639; % lbm/s
 Param.OF = 1.4;
-Param.massFrac = 0.75;
-Param.mdotFUS = Param.mdotTotUS * Param.massFrac; % lbm/s
-Param.mdotOXUS = Param.mdotTotUS * (1 - Param.massFrac); % lbm/s
+Param.mdotFUS = Param.mdotTotUS/(1+Param.OF); % lbm/s
+Param.mdotOXUS = (Param.mdotTotUS*Param.OF)/(1+Param.OF); % lbm/s
 
     % SI
 Param.Pc = convpres(Param.PcUS, 'psi', 'Pa'); % Pa
@@ -51,76 +52,123 @@ Ox.dP = Ox.stiffness*Param.Pc;
 Fuel.manifoldPressure = Param.Pc + Fuel.dP;
 Ox.manifoldPressure = Param.Pc + Ox.dP;
 
-% Chosen Parameters
-
-Ox.sprayConeHalfAngle = 40; %30-40 deg
-Fuel.numInPass = 3;
-Ox.numInPass = 3;
-Fuel.inLengthFactor = 3; % 3-6, factor to mult inlet radius for inlet length
-Ox.inLengthFactor = 3; 
-Fuel.nozLengthFactor = 0.5; % 0.5-2 factor to mult nozzle radius for nozzle length
-Ox.nozLengthFactor = 0.5;
-Fuel.chamLengthFactor = 3; % >2 factor to mult inlet radius vortex-chamber length
-Ox.chamLengthFactor = 3;
-Fuel.RIn = 3; % Radial distance from centerline to inlet center
-Ox.RInFactor = 3; % May want to iterate to get desired spray angle
-
 % Fluid Properties
 
 Fuel.temp = 293; % Change depending on regen script
 Ox.temp = 90; % Change depending on predicted LOX temp
 Fuel.viscosity = py.CoolProp.CoolProp.PropsSI('V', 'T', Fuel.temp, 'P', Fuel.manifoldPressure, 'Ethanol');
 Ox.viscosity = py.CoolProp.CoolProp.PropsSI('V', 'T', Ox.temp, 'P', Ox.manifoldPressure, 'Oxygen');
+Fuel.rho = py.CoolProp.CoolProp.PropsSI('D', 'T', Fuel.temp, 'P', Fuel.manifoldPressure, 'Ethanol'); 
 Ox.rho = py.CoolProp.CoolProp.PropsSI('D', 'T', Ox.temp, 'P', Ox.manifoldPressure, 'Oxygen'); 
 
-% Array Initialization
-Array.AOx = [];
-Array.muOx = [];
-Array.RnOx = [];
+% Chosen Parameters
+
+Ox.sprayConeHalfAngle = 50;
+Fuel.sprayConeHalfAngle = Ox.sprayConeHalfAngle - 7.5;
+Fuel.numInPass = 2;
+Ox.numInPass = 8;
+Fuel.inLengthFactor = 3; % 3-6, factor to mult inlet radius for inlet length
+Ox.inLengthFactor = 3; 
+Fuel.nozLengthFactor = 0.5; % 0.5-2 factor to mult nozzle radius for nozzle length
+Ox.nozLengthFactor = 2;
+Fuel.chamLengthFactor = 2; % >2 factor to mult inlet radius vortex-chamber length
+Ox.chamLengthFactor = 2;
+Fuel.RInFactor = 3; % Radial distance from centerline to inlet center; affects nozzle/chamber length ratio; 3 for closed, 0.7-0.8 for open
+Ox.RInFactor = 0.7;
 
 %% Main
 
+report = 'swirlOutputGeo.txt';
 Ox = stageOneParam(Ox, Chart);
-disp(Ox);
+Fuel = stageOneParam(Fuel, Chart);
+checkReportGeometry(Ox, Fuel, report, Param);
 
 %% Functions
-function Ox = stageOneParam(Ox, Chart)
+function species = stageOneParam(species, Chart)
     
-    Atol = 10^-10;
+    Atol = 0.1;
     AError = realmax;
-    AGuess = interp1(Chart.alpha, Chart.AQalpha, Ox.sprayConeHalfAngle, 'pchip');
+    AGuess = interp1(Chart.alpha, Chart.AQalpha, species.sprayConeHalfAngle, 'pchip');
     while (abs(AError) > Atol)
-        Ox.mu = interp1(Chart.AQmu, Chart.mu, AGuess, 'pchip'); % Mass flow coefficient
-        Ox.Rn = 0.475 * sqrt(Ox.mdot / (Ox.mu...
-            * sqrt(Ox.rho * Ox.dP))); % Nozzle radius (103)
-        Ox.Rin = Ox.RInFactor * Ox.Rn; % Radial distance from centerline to inlet center
-        Ox.rIn = sqrt((Ox.Rin * Ox.Rn) / (Ox.numInPass * AGuess)); % Inlet passage radius (104)
-        Ox.inletLength = Ox.rIn * Ox.inLengthFactor; % Inlet length
-        Ox.nozzleLength = Ox.Rn * Ox.nozLengthFactor; % Nozzle length
-        Ox.chamLength = Ox.Rin * Ox.chamLengthFactor; % Vortex chamber length
+        species.mu = interp1(Chart.AQmu, Chart.mu, AGuess, 'pchip'); % Mass flow coefficient
+        species.Rn = 0.475 * sqrt(species.mdot / (species.mu...
+            * sqrt(species.rho * species.dP))); % Nozzle radius (103)
+        species.RIn = species.RInFactor * species.Rn; % Radial distance from centerline to inlet center
+        species.rIn = sqrt((species.RIn * species.Rn) / (species.numInPass * AGuess)); % Inlet passage radius (104)
+        species.inletLength = species.rIn * species.inLengthFactor; % Inlet length
+        species.nozzleLength = species.Rn * species.nozLengthFactor; % Nozzle length
+        species.chamLength = species.RIn * species.chamLengthFactor; % Vortex chamber length
     
-        Ox.Rs = Ox.Rin + Ox.rIn; % Vortex chamber radius
+        species.Rs = species.RIn + species.rIn; % Vortex chamber radius
 
-        Ox.ReIn = 0.637 * (Ox.mdot / (sqrt(Ox.numInPass) * Ox.rIn...
-            * Ox.viscosity)); % Reynolds in inlet passages
+        species.ReIn = 0.637 * (species.mdot / (sqrt(species.numInPass) * species.rIn...
+            * species.viscosity)); % Reynolds in inlet passages
 
-        Ox.lambda = 0.3164 / (Ox.ReIn)^0.25; % Friction factor
+        species.lambda = 0.3164 / (species.ReIn)^0.25; % Friction factor
 
-        Ox.AEq = (Ox.Rin * Ox.Rn) / (Ox.numInPass * Ox.rIn^2 +...
-            Ox.lambda * Ox.Rin * (Ox.Rin - Ox.Rn)*0.5); % A corrected for viscous losses (100)
-        Ox.tilt = 90 - atand(Ox.Rs / Ox.inletLength); % Inlet tilting angle
-        Ox.muEq = interp1(Chart.AQmu, Chart.mu, Ox.AEq, 'pchip'); % Mass flow coefficient corrected for viscous losses
-        Ox.alphaEq = interp1(Chart.AQalpha, Chart.alpha, Ox.AEq, 'pchip');% Spray cone angle coefficient corrected for viscous losses
-        Ox.xiIn = -(0.4 * Ox.tilt)/60 + 0.9; % Hydrolic-loss coefficient (Fig. 25)
-        Ox.xi = Ox.xiIn + Ox.lambda * (Ox.inletLength / (2 * Ox.rIn));
+        species.AEq = (species.RIn * species.Rn) / (species.numInPass * species.rIn^2 +...
+            species.lambda * species.RIn * (species.RIn - species.Rn)*0.5); % A corrected for viscous losses (100)
+        species.tilt = 90 - atand(species.Rs / species.inletLength); % Inlet tilting angle
+        species.muEq = interp1(Chart.AQmu, Chart.mu, species.AEq, 'pchip'); % Mass flow coefficient corrected for viscous losses
+        species.alphaEq = interp1(Chart.AQalpha, Chart.alpha, species.AEq, 'pchip');% Spray cone angle coefficient corrected for viscous losses
+        species.xiIn = -(0.4 * species.tilt)/60 + 0.9; % Hydrolic-loss coefficient (Fig. 25)
+        species.xi = species.xiIn + species.lambda * (species.inletLength / (2 * species.rIn));
     
-        Ox.mu = Ox.muEq / sqrt(1 +...
-            Ox.xi * Ox.muEq^2 * AGuess^2 / Ox.RInFactor^2);
-        Ox.Rn = 0.475 * sqrt(Ox.mdot / (Ox.mu...
-            * sqrt(Ox.rho * Ox.dP)));
-        Ox.A = (Ox.Rin * Ox.Rn) / (Ox.numInPass * Ox.rIn^2);
+        species.mu = species.muEq / sqrt(1 +...
+            species.xi * species.muEq^2 * AGuess^2 / species.RInFactor^2);
+        species.Rn = 0.475 * sqrt(species.mdot / (species.mu...
+            * sqrt(species.rho * species.dP)));
+        ANextGuess = (species.RIn * species.Rn) / (species.numInPass * species.rIn^2);
 
-        AError = AGuess - Ox.A;
-        AGuess = Ox.A + 0.1*AError;
+        AError = AGuess - ANextGuess;
+        AGuess = ANextGuess;
     end
+
+    species.length = species.nozzleLength + species.chamLength;
+end
+
+function checkReportGeometry(Ox, Fuel, report, Param)
+    reason = {};
+
+    if (Ox.Rn + Param.wallThickness < Fuel.Rn)
+        speciesStageOne = 'Ox';
+        speciesStageTwo = 'Fuel';
+        if (Ox.length - Fuel.length - 2*Ox.rIn < Param.minFlangeThickness)
+            pass = false;
+            reason = [reason, {'Flange Thickness Violated'}];
+        else
+            pass = true;
+        end
+    elseif (Fuel.Rn + Param.wallThickness < Ox.Rn)
+        speciesStageOne = 'Fuel';
+        speciesStageTwo = 'Ox';
+        if (Fuel.length - Ox.length - 2*Fuel.rIn < Param.minFlangeThickness)
+            pass = false;
+            reason = [reason, {'Flange Thickness Violated'}];
+        else
+            pass = true;
+        end
+    else
+        pass = false;
+        reason = [reason, {'Nozzle Collision'}];
+        speciesStageOne = 'N/A';
+        speciesStageTwo = 'N/A';
+    end
+
+    reportWrite = fopen(report, 'w');
+
+    if (pass == false)
+        for i = 1:numel(reason)
+            fprintf(reportWrite, 'NO PASS:\n%s\n' ,reason{i});
+        end
+        fprintf(reportWrite, '\n');
+    end
+    fprintf(reportWrite, 'Stage One: %s\nStage Two: %s\n', speciesStageOne,...
+        speciesStageTwo);
+    fprintf(reportWrite, '\nOX DIMENSIONS\nNozzle Radius: %f m\nChamber Radius: %f m\nInlet Radius: %f m\nInlet Tilt: %f deg\nNumber of Inlet Passes: %f\nInlet Position: %f m\nInlet Length: %f m\nNozzle Length: %f m\nChamber Length: %f m\nA Parameter: %f\n', ...
+        Ox.Rn, Ox.Rs, Ox.rIn, Ox.tilt, Ox.numInPass, Ox.RIn, ...
+        Ox.inletLength, Ox.nozzleLength, Ox.chamLength, Ox.AEq);
+    fprintf(reportWrite, '\nFUEL DIMENSIONS\nNozzle Radius: %f m\nChamber Radius: %f m\nInlet Radius: %f m\nInlet Tilt: %f deg\nNumber of Inlet Passes: %f\nInlet Position: %f m\nInlet Length: %f m\nNozzle Length: %f m\nChamber Length: %f m\nA Parameter: %f\n', ...
+        Fuel.Rn, Fuel.Rs, Fuel.rIn, Fuel.tilt, Fuel.numInPass, Fuel.RIn, ...
+        Fuel.inletLength, Fuel.nozzleLength, Fuel.chamLength, Fuel.AEq);
 end
